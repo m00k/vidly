e1aa075345e90baa33e07b7e8e7bd38be0a17752
m00k <christian@noenv.com>
2018-12-01 16:48:12 +0100

    feat(movies): create header

--- src/movies.jsx
+++ src/movies.jsx
@@ -2,34 +2,41 @@ import React, { Component } from "react";
 import { getMovies } from "./services/fakeMovieService";
 
 class Movies extends Component {
-    state = {
-        movies: getMovies()
-    };
+  state = {
+    movies: getMovies()
+  };
 
-    render() {
-        return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Rate</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.movies.map(movie => (
-                        <tr key={movie._id}>
-                            <td>{movie.title}</td>
-                            <td>{movie.genre.name}</td>
-                            <td>{movie.numberInStock}</td>
-                            <td>{movie.dailyRentalRate}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        );
-    }
+  render() {
+    const header = this.state.movies.length
+        ? <h2>Showing {this.state.movies.length} movies in the database</h2>
+        : <h2>Enter some movies!</h2>;
+
+    return (
+      <React.Fragment>
+        {header}
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Movies;
