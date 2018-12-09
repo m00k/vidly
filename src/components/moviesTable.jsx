14feed362b3c752a1b618d164b65fe046e14a473
m00k <christian@noenv.com>
2018-12-09 08:35:44 +0100

    refactor: extract table component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from './common/tableBody';
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -34,19 +33,13 @@ class MoviesTable extends Component {
     const { movies, sortColumn, onSort } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        >
-        </TableHeader>
-        <TableBody
-          data={movies}
-          columns={this.columns}
-        >
-        </TableBody>
-      </table>
+      <Table
+        data={movies}
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      >
+      </Table>
     );
   }
 }
