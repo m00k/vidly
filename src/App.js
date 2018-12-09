bf5f849395275c8d5df2a9edcab033ab25f9d80a
m00k <christian@noenv.com>
2018-12-09 17:34:54 +0100

    feat(navbar): add navbar

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Movies from './components/movies';
 import NotFound from './components/notFound';
 import Customers from './components/costumers';
 import Rentals from './components/rentals';
+import NavBar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/costumers" component={Customers}/>
-          <Route path="/rentals" component={Rentals}/>
-          <Route path="/movies" component={Movies}/>
-          <Route path="/not-found" component={NotFound}/>
-          <Redirect from="/" to="/movies" exact component={Movies}/>
-          <Redirect to="/not-found"/>
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/costumers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" to="/movies" exact component={Movies} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
