c939916e5684e71f28e6f1e25379a243dd558fd1
m00k <christian@noenv.com>
2018-12-09 17:20:24 +0100

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Movies from './components/movies';
+import NotFound from './components/notFound';
+import Customers from './components/costumers';
+import Rentals from './components/rentals';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies>
-        </Movies>
+        <Switch>
+          <Route path="/costumers" component={Customers}/>
+          <Route path="/rentals" component={Rentals}/>
+          <Route path="/movies" component={Movies}/>
+          <Route path="/not-found" component={NotFound}/>
+          <Redirect from="/" to="/movies" exact component={Movies}/>
+          <Redirect to="/not-found"/>
+        </Switch>
       </main>
     );
   }
---
+++ src/components/movieForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MovieForm = ({match}) => {
+  return (
+    <h1>MovieForm - {match.params.id}</h1>
+  );
+}
+ 
+export default MovieForm;
